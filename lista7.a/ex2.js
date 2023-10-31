1b29bbb8468a8f432829d8d72cc32910643da71e
leonardo A. de Barros <142343576+Leonardoabarros@users.noreply.github.com>
2023-10-30 22:53:29 -0300

    Add files via upload

--- lista7.a/ex2.js
+++ lista7.a/ex2.js
@@ -1,22 +1,35 @@
-function calcula1220(matriz){
-    let qtde = 0
-    for(let i=0;i<2;i++){
+function lerMatriz(matriz) {
+    for (let i = 0; i < 2; i++) {
         matriz[i] = []
-         qtde = 0
-         for(let j=0;j<4;j++){
-                matriz[i][j] = parseInt((Math.random()*20)+1)
-             if (matriz[i][j] >= 12 && matriz[i][j] <= 20){
-                 qtde++
-             } 
-         }
-         console.log(`A quantidade entre 12 e 20 na linha ${i} é ${qtde}`)
+        for (let j = 0; j < 4; j++) {
+            matriz[i][j] = parseInt((Math.random() * 20) + 1)
+        }
     }
-   
- }
- let principal = () => {
+}
+function calcula1520(matriz) {
+    let media = 0
+    let cont = 0
+    for (let i = 0; i < 2; i++) {
+        let qtde = 0
+            for (let j = 0; j < 4; j++) {
+            if (matriz[i][j] >= 15 && matriz[i][j] <= 20) {
+                qtde++
+            }
+            if (matriz[i][j] % 2 == 0) {
+                media = media + matriz[i][j]
+                cont++
+            }
+        }
+        console.log(`A quantidade de numeros pares na linha ${i+1} e ${qtde}`)
+    }
+    console.log(`A quantidade de elementos pares e ${cont}`)
+    console.log(`A soma dos elementos pares e ${media}`)
+    console.log(`A media dos números pares é ${media / cont}`)
+}
+let principal = () => {
     let matriz = []
-   
-    calcula1220(matriz)
+    lerMatriz(matriz)
+    calcula1520(matriz)
     console.log(matriz)
 }
 principal()
